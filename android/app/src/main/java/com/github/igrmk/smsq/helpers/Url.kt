b8689c317696f8ba2931e9bde88dc6ca7fe0e51d
Georgy <tarasverq@gmail.com>
2023-08-17 22:55:47 +0700

    Update Url.kt

--- android/app/src/main/java/com/github/igrmk/smsq/helpers/Url.kt
+++ android/app/src/main/java/com/github/igrmk/smsq/helpers/Url.kt
@@ -3,5 +3,5 @@ package com.github.igrmk.smsq.helpers
 import com.github.igrmk.smsq.BuildConfig
 
 @Suppress("ConstantConditionIf")
-fun apiUrl(baseUrl: String) = if (BuildConfig.BUILD_TYPE == "staging") "https://apitest.$baseUrl/v1" else "https://api.$baseUrl/v1"
+fun apiUrl(baseUrl: String) = if (BuildConfig.BUILD_TYPE == "staging") "https://$baseUrl/v1" else "https://$baseUrl/v1"
 fun postSmsUrl(baseUrl: String) = "${apiUrl(baseUrl)}/sms"
